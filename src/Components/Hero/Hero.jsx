f6faa5c7910fcef74cef3e37260cdead03c07ae3
Ayushi Kumari <111629528+ayushiydu@users.noreply.github.com>
2025-04-21 12:08:36 +0530

    Add files via upload

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_img} alt="" className='profile-img' />
+      <h1><span>I'm Ayushi Kumari,</span> frontend developer based in INDIA.</h1>
+      <p>I am a frontend developer in INDIA.
+      "Experienced in handling diverse projects, including a successful internship developing a feature-rich job portal for a tech company."</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,26 @@
+
+
+
+import project4_img from '../assets/project_4.svg'
+import project5_img from '../assets/project_5.svg'
+import project6_img from '../assets/project_6.png'
+
+const mywork_data = [
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project4_img
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project6_img
+    },
+]
+ 
+export default mywork_data;
